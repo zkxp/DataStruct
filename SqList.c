eb53d5d2c1eb51266f077295dfb6dfb0161ad8c5
zkxp <zkxp2011@qq.com>
2013-12-19 17:25:17 +0800

    修改

--- SqList.c
+++ SqList.c
@@ -7,39 +7,98 @@
 
 
 typedef  int ElemType;
-typedef struct{
+typedef struct
+{
     ElemType *elem;
     int length;
     int listsize;
-}SqList;
+} SqList;
 
-int Init_SqList(SqList *L){
+int Init_SqList(SqList *L)
+{
     (*L).elem=(ElemType *)malloc(LIST_SIZE*sizeof(ElemType));
     if(!(*L).elem ) exit(OVERFLOW );
     (*L).length=0;
     (*L).listsize=LIST_SIZE;
     return OK;
 }
-void Create_SqList(SqList *L,int n ){
+
+void Create_SqList(SqList *L,int n )
+{
     int i;
-    for(i=0;i<n;i++){
+    for(i=0; i<n; i++)
+    {
         scanf("%d",&(*L).elem[i]);
         (*L).length++;
     }
 }
-void DisPlay_SqList(SqList *L){
+void Create_SqListByArray(SqList *L,int data[],int k){
     int i;
-    for (i=0;i<(*L).length;i++){
+    for(i=0;i<k;i++){
+        (*L).elem[i]=data[i];
+        (*L).length++;
+    }
+}
+void DisPlay_SqList(SqList *L)
+{
+    int i;
+    for (i=0; i<(*L).length; i++)
+    {
         printf("%d ",(*L).elem[i]);
 
     }
-    printf("\n ");
+    printf("\n");
+}
+void Destroy_SqList(SqList *L)
+{
+    free((*L).elem) ;
+}
+int SqList_Insert(SqList *L,int i,ElemType e)
+{
+    int j;
+    ElemType * p;
+    if(i<1||i>(*L).length) return ERROR;
+    if((*L).length>=(*L).listsize)
+    {
+        p=(ElemType*)realloc((*L).elem,((*L).listsize+LIST_INCREMENT)*sizeof(ElemType));
+        if(!p) exit(OVERFLOW);
+        (*L).elem=p;
+        (*L).listsize=(*L).listsize+LIST_INCREMENT;
+    }
+    for(j=(*L).length-1;j>=i-1;j--){
+        (*L).elem[j+1]=(*L).elem[j];
+    }
+    (*L).length++;
+    (*L).elem[i-1]=e;
+
+    return OK;
+
+}
+//删除指定位置元素，L.elem[j]=L.elem[j+1]
+int SqList_delete(SqList *L,int i){
+    int j;
+    for(j=i-1;j<(*L).length-1;j++){
+        (*L).elem[j]=(*L).elem[j+1];
+    }
+    (*L).length--;
 }
 int main()
 {
     SqList L;
+    int data[5];
+//    data={3,23,54,9,45};
+    data[0]=3,data[1]=83,data[2]=24,data[3]=12,data[4]=48;
     Init_SqList(&L);
-    Create_SqList(&L,5);
+    Create_SqListByArray(&L,data,5);
+    DisPlay_SqList(&L);
+    printf("input location,element:");
+    int e,i,m;
+    scanf("%d,%d",&i,&e);
+    SqList_Insert(&L,i,e);
+    DisPlay_SqList(&L);
+    printf("input location where you want delet:");
+    scanf("%d",&m);
+    SqList_delete(&L,m);
     DisPlay_SqList(&L);
     return 0;
 }
